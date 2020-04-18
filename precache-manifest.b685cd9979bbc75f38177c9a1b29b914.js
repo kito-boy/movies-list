5a461cb8b4711ba68eaf754b22efd3e74372b6b3
kitoboy <>
2020-04-18 15:57:55 +0300

    Updates

---
+++ precache-manifest.b685cd9979bbc75f38177c9a1b29b914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105fc0db9e3a3cbeef31eacd41c720d8",
+    "url": "/movies-list/index.html"
+  },
+  {
+    "revision": "07bf1d819801c0e0a4b4",
+    "url": "/movies-list/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d6b8f83f8e8db8b937d9",
+    "url": "/movies-list/static/css/main.300e9410.chunk.css"
+  },
+  {
+    "revision": "07bf1d819801c0e0a4b4",
+    "url": "/movies-list/static/js/2.b5f64d60.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movies-list/static/js/2.b5f64d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b8f83f8e8db8b937d9",
+    "url": "/movies-list/static/js/main.8c38e493.chunk.js"
+  },
+  {
+    "revision": "4d2f3343278df379a427",
+    "url": "/movies-list/static/js/runtime-main.83b23d88.js"
+  }
+]);
